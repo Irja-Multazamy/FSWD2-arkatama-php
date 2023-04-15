c76e7203c70504e468abd2ea8685ebd2acb89fd8
Irja-Multazamy <irjamultazami@gmail.com>
2023-04-15 12:35:41 +0700

    update tugas

---
+++ biodata.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Biodata | Irja' Multazamy</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp"
+    crossorigin="anonymous">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap" rel="stylesheet">
+    <style>
+        body{
+            background-color: aliceblue;
+        }
+        .card {
+            border: none;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="pt-3 text-center"><h2>BIODATAKU</h2></div>
+        <!-- card -->
+        <div class="d-flex justify-content-around">
+            <div class="col-lg-10 col-md-7 p-4">
+                <div class="card bg-white text-dark border-0">
+                    <div class="card-body">
+                        <table>
+                            <tr>
+                                <td rowspan="7"><img src="img/me.jpg" alt="" class="rounded me-4" width="180px"></td>
+                            </tr>
+                            <tr>
+                                <td><b>Nama Lengkap</b></td>
+                                <td width="20px" align="center">:</td>
+                                <td>Irja' Multazamy</td>
+                            </tr>
+                            <tr>
+                                <td><b>Tempat, Tanggal Lahir</b></td>
+                                <td align="center">:</td>
+                                <td>Bangkalan, 07 Juli 2001</td>
+                            </tr>
+                            <tr>
+                                <td><b>Jenis Kelamin</b></td>
+                                <td align="center">:</td>
+                                <td>Laki-laki</td>
+                            </tr>
+                            <tr>
+                                <td><b>Alamat Lengkap</b></td>
+                                <td align="center">:</td>
+                                <td>Ds. Pangeran Geddungan, Kec. Blega, Kab. Bangkalan, Prov. Jawa Timur</td>
+                            </tr>
+                            <tr>
+                                <td><b>Riwayat Pendidikan</b></td>
+                                <td align="center">:</td>
+                                <td>Teknik Komputer & Jaringan - SMK Almas'udiyah<br>S1 Teknik Informatika - Universitas Trunojoyo Madura</td>
+                            </tr>
+                            <tr>
+                                <td><b>Hobi</b></td>
+                                <td align="center">:</td>
+                                <td>Nongkrong & nonton anime</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
+    crossorigin="anonymous"></script>
+</body>
+</html>
